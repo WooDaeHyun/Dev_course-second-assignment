2f226db1f8b3506ea22b4d2d44e4dd78995eeb44
WooDaeHyun <po668312@naver.com>
2022-11-07 22:28:44 +0900

    feat : header component 추가

--- src/header.js
+++ src/header.js
@@ -1,4 +1,7 @@
 export default function Header({ $target }) {
+  if (!new.target) {
+    throw new Error("new 연산자와 함께 호출해 주세요");
+  }
   const $h1 = document.createElement("h1");
   $target.appendChild($h1);
 
